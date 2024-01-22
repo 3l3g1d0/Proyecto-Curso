7e9dfb4e955527390087dce4b97a7727d69b7d32
3l3g1d0 <35312478+3l3g1d0@users.noreply.github.com>
2024-01-22 15:23:59 +0100

    Update proyecto_definitivo(1).py
    he cambiado el codigo

--- proyecto_definitivo(1).py
+++ proyecto_definitivo(1).py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import sys
 import requests
 import shutil
 
@@ -8,8 +9,8 @@ carpeta_infectados = "archivos_infectados/"
 carpeta_limpios = "archivos_limpios/"
 database_file = "virus_total.db"
 
-directory = r'C:\Users\Ivanc\Downloads\virus'
-output_folder = r'C:\Users\Ivanc\OneDrive\Documentos\archivos_infectados'
+##directory = r'C:\Users\Ivanc\Downloads\virus'
+##output_folder = r'C:\Users\Ivanc\OneDrive\Documentos\archivos_infectados'
 
 if not os.path.exists(carpeta_infectados):
     os.mkdir(carpeta_infectados)
@@ -42,7 +43,7 @@ def analizar_archivo(filepath):
         print("Archivo infectado")
         shutil.move(filepath, os.path.join(carpeta_infectados, os.path.basename(filepath)))
     else:
-        print("El archivo está limpio")
+        print("El archivo esta limpio")
      
 
 def analizar_url(url):
@@ -52,26 +53,15 @@ def analizar_url(url):
     response = requests.post(url, json=data, params=params)
     
 def main():
-    while True:
-        print("Paso 1. Analizar archivo")
-        print("Paso 2. Analizar URL")
-        print("Paso 3. Salir")
-        opcion = input("Selecciona una opción: ")
-
-        if opcion == "1":
-            filepath = input("Ruta del archivo: ")
+   
+           
+            filepath = sys.argv[1]
             if os.path.isfile(filepath):
+                
                 analizar_archivo(filepath)
             else:
                 print('Ruta de archivo no válida')
-        elif opcion == "2":
-            url = input("URL a analizar: ")
-            response_data = analizar_url(url)
-            print("Se ha enviado la URL para analizar en VirusTotal.")
-        elif opcion == "3":
-            break
-        else:
-            print("Opción no válida")
+            
 
 if __name__ == "__main__":
     main()
