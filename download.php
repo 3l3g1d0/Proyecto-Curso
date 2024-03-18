9db0db482e509f7343d34df4aac7207971d3ea54
3l3g1d0 <35312478+3l3g1d0@users.noreply.github.com>
2024-03-18 16:19:59 +0100

    Update download.php

--- download.php
+++ download.php
@@ -1,16 +1,19 @@
 <?php
     if(isset($_GET['file'])) {
-        // Ruta del archivo a descargar
+        
         $file = 'C:\\users\\cf2020236\\Downloads\\laragon\\www\\usuarios\\' . $_SESSION['nickname'] . '\\' . $_GET['file'];
 
-        // Verificar si el archivo existe y es legible
+       
+        
         if (file_exists($file) && is_readable($file)) {
-            // Establecer encabezados para forzar la descarga del archivo
+           
+            
             header('Content-Type: application/octet-stream');
             header('Content-Disposition: attachment; filename="' . basename($file) . '"');
             header('Content-Length: ' . filesize($file));
 
-            // Leer el archivo y enviarlo al cliente
+            
+            
             readfile($file);
             exit;
         } else {
