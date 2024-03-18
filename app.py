b5b637e3082ca8a56adc836aca4fc71b40fdca59
3l3g1d0 <35312478+3l3g1d0@users.noreply.github.com>
2024-03-18 16:23:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ def index():
  
 @app.route('/analizar')
 def analizar_contenido():
-    # Agrega el código para llamar a tu script de Python para el análisis
-    # Puedes llamar a la función o la lógica de análisis aquí
+
     return 'Análisis de contenido'
  
 if __name__ == '__main__':
